dec55eea5a481b02e7276e8ecd8f2ef21976500c
= <abhisingh30800@gmail.com>
2023-05-27 20:54:08 +0530

    added contact data and postcontact file

---
+++ pages/api/postcontact.js
@@ -0,0 +1,15 @@
+import * as fs from "fs";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    let data = await fs.promises.readdir("contactdata");
+    console.log(data.length);
+    res.status(200).json(req.body);
+    fs.promises.writeFile(
+      `contactdata/${data.length + 1}.json`,
+      JSON.stringify(req.body)
+    );
+  } else {
+    res.status(200).json(["allBlogs"]);
+  }
+}
